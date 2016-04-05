b4d5063a6ed86c6ba2110dcb80e49f75a64bfa1f
Mathijs Bernson <mathijs.bernson@gmail.com>
2016-04-05 14:20:27 +0200

    Add Identity monad

---
+++ src/Identity.php
@@ -0,0 +1,7 @@
+<?php namespace Duckson\Monads;
+
+class Identity extends BaseMonad implements Monad {
+    public function bind(callable $fn, ...$args) {
+        return new Identity($fn($this->value, ...$args));
+    }
+}
---
+++ tests/IdentityTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use Duckson\Monads\Identity;
+
+class IdentityTest extends PHPUnit_Framework_TestCase {
+    public function legalValuesProvider() {
+        $obj = new stdClass();
+        $obj->test = 'test';
+        return [
+            [true],
+            [false],
+            [''],
+            ['Something'],
+            [123],
+            [0],
+            [1],
+            [-1],
+            [$obj],
+        ];
+    }
+
+    /**
+     * @dataProvider legalValuesProvider
+     */
+    public function testBindOnValue($legalValue) {
+        $identity = new Identity($legalValue);
+        $value = $identity->bind(function($value) use ($legalValue) {
+            self::assertNotNull($value);
+            self::assertEquals($legalValue, $value);
+            return $value;
+        })->value();
+        $this->assertEquals($legalValue, $value);
+    }
+
+    /**
+     * @dataProvider legalValuesProvider
+     */
+    public function testBindSugarOnValue($value) {
+        $identity = new Identity($value);
+        $this->assertEquals($value, $identity->value());
+    }
+
+    public function testMultipleBindSugarOnValue() {
+        $value = new stdClass;
+        $value->test = 'test';
+        $identity = new Identity($value);
+        $this->assertEquals('test', $identity->test->value());
+    }
+}
